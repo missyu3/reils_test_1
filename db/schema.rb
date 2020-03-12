ec2a8ccc11fcbcb434bf3d23a23421fa5b252ad8
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-12 20:30:46 +0900

    新規作成画面追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_12_092110) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
