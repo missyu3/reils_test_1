c2401b61476048b1693a71aee24416c578fba63e
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-13 10:36:53 +0900

    バリデーションと表示

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,17 @@
 class ContactsController < ApplicationController
 
   def new
-
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(params.require(:contact).permit(:name,:email,:content)) 
 
+    if @contact.save
+      redirect_to contact_path, notice:"お問い合わせありがとうございます！"
+    else
+      render:new
+    end
   end
 
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ApplicationRecord
+    validates :content, presence: true
+    validates :content, length: { maximum: 140}
 end
